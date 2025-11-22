5b7bc90b4cf8cecab0c47e8648e0448df6146056
SalmanHossen <hossainsalman873@gmail.com>
2025-11-22 12:29:54 +0600

    solve count of number digit

---
+++ count_number_digit.cpp
@@ -0,0 +1,21 @@
+// Count the number of digits of a given number n.
+
+// Calculate the sum of digits of a given number n.
+
+#include <iostream>
+using namespace std;
+
+int sum_of_digit(int n){
+    // base case
+   //  if(n == 0) return 0;
+   if(n<10) return n;
+   return 1 + sum_of_digit(n/10);
+}
+
+int main() {
+   int n;
+   cout << "Enter number: ";
+   cin >> n;      
+   cout << "Sum of digit " << n << " is = " << sum_of_digit(n) << endl;
+   return 0;
+}
