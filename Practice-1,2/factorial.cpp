be266aaa0afc2d2d7f8214f7ff91f9fa469391d6
SalmanHossen <hossainsalman873@gmail.com>
2025-11-28 19:08:24 +0600

    factorial using recursion

---
+++ Practice-1,2/factorial.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+   if(n==0) return 1;
+   return n*factorial(n-1);
+}
+
+int main(){
+
+   int n;
+   cout<<"Enter your number ";
+
+   cin >> n;
+
+   cout<<"factorial is "<<factorial(n)<<endl;
+   return 0;
+}
+ 
